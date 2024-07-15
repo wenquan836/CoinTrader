869e7d0a2e6c304215a3205f406394d3a3a837de
wenquan836 <114587904@qq.com>
2024-07-15 22:43:58 +0800

    删除USDK选项

--- CoinTrader.Forms/WinCTCConfig.Designer.cs
+++ CoinTrader.Forms/WinCTCConfig.Designer.cs
@@ -63,20 +63,20 @@
             this.groupBox1.Controls.Add(this.txtCurrencies);
             this.groupBox1.Controls.Add(this.txtLoginName);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(33, 23);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(36, 26);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(541, 161);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(590, 184);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "账户设置";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(455, 86);
+            this.button2.Location = new System.Drawing.Point(496, 98);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 57);
+            this.button2.Size = new System.Drawing.Size(82, 65);
             this.button2.TabIndex = 7;
             this.button2.Text = "选择";
             this.button2.UseVisualStyleBackColor = true;
@@ -85,39 +85,39 @@
             // label24
             // 
             this.label24.AutoSize = true;
-            this.label24.Location = new System.Drawing.Point(47, 92);
+            this.label24.Location = new System.Drawing.Point(51, 105);
             this.label24.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label24.Name = "label24";
-            this.label24.Size = new System.Drawing.Size(52, 21);
+            this.label24.Size = new System.Drawing.Size(58, 24);
             this.label24.TabIndex = 6;
             this.label24.Text = "币种";
             // 
             // txtCurrencies
             // 
-            this.txtCurrencies.Location = new System.Drawing.Point(135, 86);
-            this.txtCurrencies.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCurrencies.Location = new System.Drawing.Point(147, 98);
+            this.txtCurrencies.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCurrencies.Multiline = true;
             this.txtCurrencies.Name = "txtCurrencies";
-            this.txtCurrencies.Size = new System.Drawing.Size(312, 57);
+            this.txtCurrencies.Size = new System.Drawing.Size(340, 65);
             this.txtCurrencies.TabIndex = 5;
             this.txtCurrencies.Text = "BTC;LTC;EOS;BCH;ETH;ETC;OKB";
             this.toolTip1.SetToolTip(this.txtCurrencies, "每个币种之间用分号（;）分隔， 比如BTC;ETH;LTC");
             // 
             // txtLoginName
             // 
-            this.txtLoginName.Location = new System.Drawing.Point(135, 32);
-            this.txtLoginName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLoginName.Location = new System.Drawing.Point(147, 37);
+            this.txtLoginName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtLoginName.Name = "txtLoginName";
-            this.txtLoginName.Size = new System.Drawing.Size(312, 31);
+            this.txtLoginName.Size = new System.Drawing.Size(340, 35);
             this.txtLoginName.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 38);
+            this.label2.Location = new System.Drawing.Point(39, 43);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.Size = new System.Drawing.Size(82, 24);
             this.label2.TabIndex = 0;
             this.label2.Text = "登录名";
             // 
@@ -127,22 +127,21 @@
             this.cbAnchor.FormattingEnabled = true;
             this.cbAnchor.Items.AddRange(new object[] {
             "USDT",
-            "USDC",
-            "USDK"});
-            this.cbAnchor.Location = new System.Drawing.Point(154, 44);
-            this.cbAnchor.Margin = new System.Windows.Forms.Padding(4);
+            "USDC"});
+            this.cbAnchor.Location = new System.Drawing.Point(168, 50);
+            this.cbAnchor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbAnchor.Name = "cbAnchor";
-            this.cbAnchor.Size = new System.Drawing.Size(293, 29);
+            this.cbAnchor.Size = new System.Drawing.Size(319, 32);
             this.cbAnchor.TabIndex = 2;
             this.cbAnchor.SelectedIndexChanged += new System.EventHandler(this.cbAnchor_SelectedIndexChanged);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(72, 51);
+            this.label8.Location = new System.Drawing.Point(79, 58);
             this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(73, 21);
+            this.label8.Size = new System.Drawing.Size(82, 24);
             this.label8.TabIndex = 0;
             this.label8.Text = "稳定币";
             // 
@@ -156,11 +155,11 @@
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.txtApiKey);
             this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Location = new System.Drawing.Point(624, 23);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Location = new System.Drawing.Point(681, 26);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(655, 300);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(715, 343);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "API设置";
@@ -168,10 +167,10 @@
             // rdoSimulated
             // 
             this.rdoSimulated.AutoSize = true;
-            this.rdoSimulated.Location = new System.Drawing.Point(396, 196);
-            this.rdoSimulated.Margin = new System.Windows.Forms.Padding(4);
+            this.rdoSimulated.Location = new System.Drawing.Point(432, 224);
+            this.rdoSimulated.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.rdoSimulated.Name = "rdoSimulated";
-            this.rdoSimulated.Size = new System.Drawing.Size(98, 25);
+            this.rdoSimulated.Size = new System.Drawing.Size(113, 28);
             this.rdoSimulated.TabIndex = 3;
             this.rdoSimulated.Text = "模拟盘";
             this.rdoSimulated.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -181,10 +180,10 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Checked = true;
-            this.radioButton2.Location = new System.Drawing.Point(191, 196);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Location = new System.Drawing.Point(208, 224);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(77, 25);
+            this.radioButton2.Size = new System.Drawing.Size(89, 28);
             this.radioButton2.TabIndex = 4;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "实盘";
@@ -193,64 +192,64 @@
             // 
             // txtApiSecretKey
             // 
-            this.txtApiSecretKey.Location = new System.Drawing.Point(193, 135);
-            this.txtApiSecretKey.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApiSecretKey.Location = new System.Drawing.Point(211, 154);
+            this.txtApiSecretKey.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtApiSecretKey.Name = "txtApiSecretKey";
-            this.txtApiSecretKey.Size = new System.Drawing.Size(398, 31);
+            this.txtApiSecretKey.Size = new System.Drawing.Size(434, 35);
             this.txtApiSecretKey.TabIndex = 1;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(33, 140);
+            this.label7.Location = new System.Drawing.Point(36, 160);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(109, 21);
+            this.label7.Size = new System.Drawing.Size(118, 24);
             this.label7.TabIndex = 0;
             this.label7.Text = "SecretKey";
             // 
             // txtApiPassphrase
             // 
-            this.txtApiPassphrase.Location = new System.Drawing.Point(193, 86);
-            this.txtApiPassphrase.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApiPassphrase.Location = new System.Drawing.Point(211, 98);
+            this.txtApiPassphrase.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtApiPassphrase.Name = "txtApiPassphrase";
-            this.txtApiPassphrase.Size = new System.Drawing.Size(398, 31);
+            this.txtApiPassphrase.Size = new System.Drawing.Size(434, 35);
             this.txtApiPassphrase.TabIndex = 1;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(33, 89);
+            this.label6.Location = new System.Drawing.Point(36, 102);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(85, 21);
+            this.label6.Size = new System.Drawing.Size(94, 24);
             this.label6.TabIndex = 0;
             this.label6.Text = "API密码";
             // 
             // txtApiKey
             // 
-            this.txtApiKey.Location = new System.Drawing.Point(193, 32);
-            this.txtApiKey.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApiKey.Location = new System.Drawing.Point(211, 37);
+            this.txtApiKey.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtApiKey.Name = "txtApiKey";
-            this.txtApiKey.Size = new System.Drawing.Size(398, 31);
+            this.txtApiKey.Size = new System.Drawing.Size(434, 35);
             this.txtApiKey.TabIndex = 1;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(33, 37);
+            this.label5.Location = new System.Drawing.Point(36, 42);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 21);
+            this.label5.Size = new System.Drawing.Size(82, 24);
             this.label5.TabIndex = 0;
             this.label5.Text = "ApiKey";
             // 
             // txtWebSocket
             // 
-            this.txtWebSocket.Location = new System.Drawing.Point(1625, 346);
-            this.txtWebSocket.Margin = new System.Windows.Forms.Padding(4);
+            this.txtWebSocket.Location = new System.Drawing.Point(1773, 395);
+            this.txtWebSocket.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtWebSocket.Name = "txtWebSocket";
-            this.txtWebSocket.Size = new System.Drawing.Size(398, 31);
+            this.txtWebSocket.Size = new System.Drawing.Size(434, 35);
             this.txtWebSocket.TabIndex = 1;
             this.txtWebSocket.Text = "wss://ws.okx.com:8443/ws/v5/public";
             this.txtWebSocket.Visible = false;
@@ -258,20 +257,20 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(1465, 351);
+            this.label10.Location = new System.Drawing.Point(1598, 401);
             this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(109, 21);
+            this.label10.Size = new System.Drawing.Size(118, 24);
             this.label10.TabIndex = 0;
             this.label10.Text = "WebSocket";
             this.label10.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(417, 360);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(455, 411);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(427, 108);
+            this.button1.Size = new System.Drawing.Size(466, 123);
             this.button1.TabIndex = 2;
             this.button1.Text = "保存";
             this.button1.UseVisualStyleBackColor = true;
@@ -281,20 +280,20 @@
             // 
             this.groupBox4.Controls.Add(this.cbAnchor);
             this.groupBox4.Controls.Add(this.label8);
-            this.groupBox4.Location = new System.Drawing.Point(33, 203);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.groupBox4.Location = new System.Drawing.Point(36, 232);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.groupBox4.Size = new System.Drawing.Size(541, 120);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.groupBox4.Size = new System.Drawing.Size(590, 137);
             this.groupBox4.TabIndex = 5;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "稳定币设置";
             // 
             // WinCTCConfig
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1306, 498);
+            this.ClientSize = new System.Drawing.Size(1425, 569);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtWebSocket);
@@ -302,7 +301,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "WinCTCConfig";
